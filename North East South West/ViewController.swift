30de53c52388cd38162cf527cb5079671b13f910
Safa Falaqi <sfalaqi@gmail.com>
2021-12-10 23:58:32 +0300

    Initial Commit

---
+++ North East South West/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  North East South West
+//
+//  Created by Safa Falaqi on 10/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
